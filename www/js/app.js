1339be3d724884765b1eb4b33dd092e6f14f9e96
vista-54 <168aa3c72>
2015-06-26 16:03:44 +0300

    4

--- www/js/app.js
+++ www/js/app.js
@@ -11,28 +11,20 @@
 //  var max=-100;
 
 var app = {
-    rssiArr: [],
-    uuid: 0,
-    rssi: 0
+
+    uuid:0
 };
 //alert("start");
 // Specify your beacon 128bit UUIDs here.
 var regions =
         [
-            // Estimote Beacon factory UUID.
-            {uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'},
             // Sample UUIDs for beacons in our lab.
             {uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E'},
-            {uuid: '8DEEFBB9-F738-4297-8040-96668BB44281'},
-            {uuid: 'A0B13730-3A9A-11E3-AA6E-0800200C9A66'},
-            {uuid: 'E20A39F4-73F5-4BC4-A12F-17D1AD07A961'},
-            {uuid: 'A4950001-C5B1-4B44-B512-1370F02D74DE'}
         ];
 
 // Dictionary of beacons.
 var beacons = {};
-
-scannedBeaconsArr = [];
+var scannedBeaconsArr = [];
 var avgArrayCount = 12;
 
 // Timer that displays list of beacons.
@@ -53,24 +45,58 @@ function onDeviceReady()
     startScan();
 
     // Display refresh timer.
-    updateTimer = setInterval(displayBeaconList, 1000);
+    updateTimer = setInterval(displayBeaconList, 500);
 }
+
+
+
+//====utils
+function findBeaconInArr(beacon) {
+    var count = scannedBeaconsArr.length;
+    //var beaconObj={uuid:uuid,minor:minor,major:major};
+
+    for (var i = 0; i < count; i++) {
+        var currScannedBeacon = scannedBeaconsArr[i];
+        if ((beacon.uuid == currScannedBeacon.uuid) && (beacon.minor == currScannedBeacon.minor) && (beacon.major == currScannedBeacon.major))
+        {
+            return scannedBeaconsArr[i];
+        }
+    }
+    return null;
+}
+
+
+function avgFromArray(arr) {
+    var sum = 0;
+    var count = 0;
+    for (var i in arr) {
+        sum += arr[i];
+        count++;
+    }
+    if (count > 0) {
+        return sum / count;
+    }
+    return 0;
+}
+
+
+
 
 function startScan()
 {
-
+    
     // The delegate object holds the iBeacon callback functions
     // specified below.
     var delegate = new locationManager.Delegate();
     // Called continuously when ranging beacons.
-
+  
     delegate.didRangeBeaconsInRegion = function (pluginResult)
     {
-        
-        var maxRSSI = -100;
+       var maxRSSI=-100;
+       var tempScannedBeakons = [];
         for (var i in pluginResult.beacons)
         {
-
+            
 //           if(maxRSSI<pluginResult.beacons[i].rssi)
 //           {
 //               maxRSSI=pluginResult.beacons[i].rssi;
@@ -78,13 +104,34 @@ function startScan()
 //           }
             var beacon = pluginResult.beacons[i];
             beacon.timeStamp = Date.now();
-
+            
             var key = beacon.uuid + ':' + beacon.major + ':' + beacon.minor;
+            
             beacons[key] = beacon;
+            
+            
+            var relevantBeacon = findBeaconInArr(beacon);
+            if( relevantBeacon!==null ){
+                    if (relevantBeacon.avgArray.length >= avgArrayCount) {
+                        relevantBeacon.avgArray.shift();
+                    }
+            }else{
+                  relevantBeacon = {uuid: beacon.uuid, major: beacon.major, minor: beacon.minor, 
+                      rssi: beacon.rssi, accuracy: 0, avgArray:[] };
+                  
+            }
+            relevantBeacon.avgArray.push(beacon.accuracy);
+            relevantBeacon.accuracy = avgFromArray(relevantBeacon.avgArray);
+            
+            tempScannedBeakons.push(relevantBeacon);
         }
+        
+        scannedBeaconsArr = tempScannedBeakons;
+    
+                        
 //        max=maxRSSI;
     };
-
+    
 
     // Called when starting to monitor a region.
     // (Not used in this example, included as a reference.)
@@ -94,7 +141,7 @@ function startScan()
     };
 
     // Called when monitoring and the state of a region changes.
-    // (Not used in this example, include3d as a reference.)
+    // (Not used in this example, included as a reference.)
     delegate.didDetermineStateForRegion = function (pluginResult)
     {
         //console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult))
@@ -132,110 +179,43 @@ function startScan()
 //        count=count+1;
 
     }
-
-}
-
-function findBeaconInArr(uuid, minor, major) {
-    var count = scannedBeaconsArr.length;
-    //var beaconObj={uuid:uuid,minor:minor,major:major};
-
-    for (var i = 0; i < count; i++) {
-        var currScannedBeacon = scannedBeaconsArr[i];
-        if ((uuid == currScannedBeacon.uuid) && (minor == currScannedBeacon.minor) && (major == currScannedBeacon.major))
-        {
-            return scannedBeaconsArr[i];
-        }
-    }
-    return null;
-}
-
-
-function avgFromArray(arr) {
-    var sum = 0;
-    var count = 0;
-    for (var i in arr) {
-        sum += arr[i];
-        count++;
-    }
-    if (count > 0) {
-        return sum / count;
-    }
-    return 0;
+  
 }
 
 
 
-function deleteLostBeaconsFromScannedArray() {
-    for (var i in scannedBeaconsArr) {
-        var currScannedBeacon = scannedBeaconsArr[i];
-        var isInAlives = false;
-        for (var j in beacons) {
-            var currAliveBeacon = beacons[j];
-            if (currAliveBeacon.uuid == currScannedBeacon.uuid
-                    && currAliveBeacon.major == currScannedBeacon.major
-                    && currAliveBeacon.minor == currScannedBeacon.minor
-            ){
-                isInAlives = true;
-            }
-        }
-        if(!isInAlives){
-            scannedBeaconsArr.splice(i,1);
-        }else{
-            aaaaa=1
-        }
-    }
-}
+
+
 
 function displayBeaconList()
 {
     // Clear beacon list.
-
-
-
-//   function ibeacon(){ 
     $('#found-beacons').empty();
     $('#info').empty();
-
+    
     var timeNow = Date.now();
-    var rM = -100;
-    var uuid = 0;//uuid start =0
-
-  
-
-
-    $.each(beacons, function (key, beacon)
+    
+    var rM=-100;//min value of rssi
+//    var majorMax=0;//
+//    var minorMax=0;
+    var uuid=0;//uuid start =0
+    // Update beacon list.
+      $.each(beacons, function (key, beacon)
     {
         //The cycle find max value of rssi and get this uuid.
-        if (beacon.rssi > rM)
+        if(beacon.rssi>rM)
         {
-            rM = beacon.rssi;
+            rM=beacon.rssi;
 //            majorMax=beacon.major;
 //            minorMax=beacon.minor;
-            uuid = beacon.uuid;
+            uuid=beacon.uuid;
         }
     });
     //function changeInformation
-    FindBeaconInDataBase(uuid);
-    deleteLostBeaconsFromScannedArray();
-
-
+     FindBeaconInDataBase(uuid);
     $.each(beacons, function (key, beacon)
     {
-        var res = findBeaconInArr(beacon.uuid, beacon.minor, beacon.major);
-        if (res==null) {
-        var beaconObj={uuid:beacon.uuid,minor:beacon.minor,major:beacon.major,rssi:beacon.rssi,accuracy:beacon.accuracy};
-            scannedBeaconsArr.push(beaconObj);
-        } else {
-            if (!res.avgArray) {
-                res.avgArray = [];
-            }
-            if (res.avgArray.length >= avgArrayCount) {
-                res.avgArray.shift();
-            }
-            res.avgArray.push(beacon.accuracy);
-            res.accuracy = avgFromArray(res.avgArray);
-            beacon.avgAccuracy=res.accuracy;
-        }
+
         // Only show beacons that are updated during the last 60 seconds.
         if (beacon.timeStamp + 60000 > timeNow)
         {
@@ -247,31 +227,8 @@ function displayBeaconList()
             else if (beacon.rssi < 0) {
                 rssiWidth = 100 + beacon.rssi;
             }
-            //=================
-
-//                if(beacon.rssi===0)
-//                {
-//                    return -1;
-//                }
-//                var ratio=beacon.rssi*1/txPower;
-//                if(ratio<1){
-//                    return pow(ratio,10);
-//                }
-//                else{
-//                    var accuracy=(0.89976)*pow(ratio,7.7095)+0.111;
-//                    return accuracy;
-//                }
-//            
-
-            //=================
-            var distance = 0;
-            if (beacon.accuracy < 1) {
-                distance = beacon.accuracy;
-            }
-            if (beacon.accuracy > 1) {
-                distance = beacon.accuracy * 1.2;
-            }
-            // distance=Math.pow(10,d);
+            
+            // Create tag to display beacon data.
             var element = $(
                     '<li>'
                     + '<strong>UUID: ' + beacon.uuid + '</strong><br />'
@@ -279,8 +236,7 @@ function displayBeaconList()
                     + 'Minor: ' + beacon.minor + '<br />'
                     + 'Proximity: ' + beacon.proximity + '<br />'
                     + 'RSSI: ' + beacon.rssi + '<br />'
-//                    + 'RSSI-maX: ' + rM + '<br />'
-                    + 'Distance: ' + beacon.accuracy +'; -- avg: '+ beacon.avgAccuracy + '<br />'
+                    + 'RSSI-maX: ' + rM + '<br />'
 //                    + 'Max major:' + majorMax + '<br/>'
 //                    + 'Max minor:' + minorMax + '<br/>'
                     + '<div style="background:rgb(255,128,64);height:20px;width:'
@@ -289,14 +245,12 @@ function displayBeaconList()
                     );
             $('#warning').remove();
             $('#found-beacons').append(element);
-
+           
 
         }
-
+      
     });
-    
-    //beacons={};
-    var beaconsWithRadiuses = buildBeaconsWithRadiusesArray(scannedBeaconsArr, existedBeaconsArr);
+      var beaconsWithRadiuses = buildBeaconsWithRadiusesArray(scannedBeaconsArr, existedBeaconsArr);
     var realPosition = corelateResult(beaconsWithRadiuses);
     
     $('#cordinate').html(realPosition.lat + " " + realPosition.lng);
@@ -306,18 +260,3 @@ function displayBeaconList()
 //};
 
 //app.initialize();
-//function add(){
-//    for(var i=0;i<5;i++)
-//    {
-//           window.locationManager = cordova.plugins.locationManager;
-////    alert("start scan");
-//    // Start tracking beacons!
-//    startScan();
-//
-//    // Display refresh timer.
-//    updateTimer = setInterval(displayBeaconList, 500);
-//         app.rssiArr.push(app.rssi);  
-//    }
-//   
-//  
-//}
--- www/js/triangulation.js
+++ www/js/triangulation.js
@@ -5,14 +5,21 @@ var existedBeaconsArr = [
     {name: 'c5nr', lat: 0, lng: 4.2, uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E', major: '46609', minor: '33951'},
 ];
 
-//
+
 //var scannedBeaconsArr = [
 ////    //  uuid, major, minor, rssi
-//    {uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E', major: '14575', minor: '21386', rssi: -82, accuracy: /*8.2*/ 10},
+//    {uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E', major: '14575', minor: '21386', rssi: -82, accuracy: 8.2 /*10*/},
 //    {uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E', major: '46650', minor: '37051', rssi: -76, accuracy: 4.2},
 //    {uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E', major: '46609', minor: '33951', rssi: -62, accuracy: 7}
 //];
 
+
+//var beaconsWithRadiuses = [
+//    {lng: -2, lat: 3, radius: 4.1},
+//    {lng: 4, lat: 2, radius: 5.8},
+//    //{lng: 4, lat: 2, radius: 7.8},
+//    {lng: 1, lat: 5, radius: 6.32}
+//];
 
 function buildBeaconsWithRadiusesArray(scannedBeaconsArr, existedBeaconsArr) {
     var beaconsWithRadiuses = [];
@@ -66,16 +73,26 @@ function findIntersectDotsByTwoCircles(P1, P2) {  //Px{ lat, lng, radius }
 }
 
 
-//var beaconsWithRadiuses = [
-//    {lng: -2, lat: 3, radius: 4.1},
-//    {lng: 4, lat: 2, radius: 5.8},
-//    //{lng: 4, lat: 2, radius: 7.8},
-//    {lng: 1, lat: 5, radius: 6.32}
-//];
-
 function detectRealPosition(beaconsWithRadiuses) {
     var interDots = [];
     var lines = [];
+
+
+    //only one beacon
+    if (lines.length == 0) {
+        var beacon = beaconsWithRadiuses[0];
+        var dot = {
+            lng: beacon.lat,
+            lat: beacon.lng,
+            probability: 0.1,
+            avgRadius: beacon.radius,
+            maxDistance: beacon.radius,
+            minDistance: 0
+        };
+        return dot;
+    }
+
+
     for (var i = 0; i < beaconsWithRadiuses.length; i++) {
         for (var j = i; j < beaconsWithRadiuses.length; j++) {
             if (i != j) {
@@ -87,20 +104,21 @@ function detectRealPosition(beaconsWithRadiuses) {
                     dotsPair.R2 = beaconsWithRadiuses[j].radius;
                     lines.push(dotsPair);  // its line
 
-                }else{
+                } else {
                     // not intersects
-                    console.log('not intersects: '+beaconsWithRadiuses[i]+ ' with '+beaconsWithRadiuses[j]);
+                    //console.log('not intersects: '+beaconsWithRadiuses[i]+ ' with '+beaconsWithRadiuses[j]);
+                    return null;
                 }
             }
         }
     }
 
 
-    for(var i=0; i<lines.length; i++){
-        for(var j=i; j<lines.length; j++){
-            if(i!=j){
+    for (var i = 0; i < lines.length; i++) {
+        for (var j = i; j < lines.length; j++) {
+            if (i != j) {
                 var interDot = getIntersectDot(lines[i], lines[j]);
-                if(interDot){
+                if (interDot) {
                     interDots.push(interDot);
                 }
 
@@ -109,8 +127,20 @@ function detectRealPosition(beaconsWithRadiuses) {
     }
 
 
+    // one line , no intersect dots
+    if (lines.length == 1) {
+        //one line is two centers of circles
+        var lineCirclesCenters = {D1: beaconsWithRadiuses[0], D2: beaconsWithRadiuses[1]};
+        lines.push(lineCirclesCenters);
+        var interDot = getIntersectDot(lines[0], lineCirclesCenters);
+        interDots.push(interDot);
+        interDots.push(interDot); // need for working algorithm
+
+    }
+
+
     var intersectDot = {};
-    var id = {lng_sum: 0, lat_sum: 0, count:0};
+    var id = {lng_sum: 0, lat_sum: 0, count: 0};
     for (var i = 0; i < interDots.length; i++) {
         for (var j = i; j < interDots.length; j++) {
             if (i != j) {
@@ -120,86 +150,69 @@ function detectRealPosition(beaconsWithRadiuses) {
             }
         }
     }
-    intersectDot.lat = id.lat_sum/id.count;
-    intersectDot.lng = id.lng_sum/id.count;
+    intersectDot.lat = id.lat_sum / id.count;
+    intersectDot.lng = id.lng_sum / id.count;
 
 
     // get minimal length;
     var nearDots = [];
-    for(var i=0; i<lines.length; i++) {
+    for (var i = 0; i < lines.length; i++) {
         var dot1len = calculateDistanceBetweenTwoDots(intersectDot, lines[i].D1);
         var dot2len = calculateDistanceBetweenTwoDots(intersectDot, lines[i].D2);
         var min = Math.min(dot1len, dot2len);
-        var nearDot = dot2len-dot1len >=0 ? lines[i].D1 :  lines[i].D2;
-        nearDot.probability = 1/min;
+        var nearDot = dot2len - dot1len >= 0 ? lines[i].D1 : lines[i].D2;
+        nearDot.probability = 1 / min;
+        nearDot.distance = min;
         nearDots.push(nearDot);
     }
 
 
-
-
-
-    //for (var i = 0; i < interDots.length; i++) {
-    //    for (var j = i; j < interDots.length; j++) {
-    //        if (i != j) {
-    //            var currDot = interDots[i];
-    //            if (!currDot.probability) {
-    //                currDot.probability = 0;
-    //            }
-    //            var distBetweenDots = calculateDistanceBetweenTwoDots(currDot, interDots[j]);
-    //            if (!isNaN(distBetweenDots)) {
-    //                currDot.probability += 1 / distBetweenDots;
-    //            }
-    //        }
-    //    }
-    //}
-
-    nearDots.sort(function (a, b) {
-        return b.probability - a.probability;
-    });
-    //
-    //var count = 0;
-    //if (interDots.length <= 2) {
-    //    count = interDots.length;
-    //} else {
-    //    count = interDots.length / 2;
-    //}
-
-
-
-    var avgLng = 0;
-    var avgLat = 0;
-    var probability = -1;
-    //for(var i=0; i<count; i++){
-    //    var currDot = interDots[i];
-    //    avgLng+=currDot.lng;
-    //    avgLat+=currDot.lat;
-    //}
-    //avgLng = //avgLng/count;
-    //avgLat = //avgLat/count;
-    var s = {lng_num: 0, lng_den: 0, lat_num: 0, lat_den: 0, sum_prob:0};
-    for (var i = 0; i <nearDots.length; i++) {
+    var s = {
+        lng_num: 0,
+        lng_den: 0,
+        lat_num: 0,
+        lat_den: 0,
+        sum_prob: 0,
+        sum_distance: 0,
+        maxDistance: 0,
+        minDistance: 1000
+    };
+    for (var i = 0; i < nearDots.length; i++) {
         var currDot = nearDots[i];
-        //currDot.probability =1;
         s.lat_num += currDot.lat * currDot.probability;
         s.lat_den += currDot.probability;
         s.lng_num += currDot.lng * currDot.probability;
         s.lng_den += currDot.probability;
-        s.sum_prob+= currDot.probability;
+        s.sum_prob += currDot.probability;
+        s.sum_distance += currDot.distance;
+        if (currDot.distance > s.maxDistance) {
+            s.maxDistance = currDot.distance;
+        }
+        if (currDot.distance < s.minDistance) {
+            s.minDistance = currDot.distance;
+        }
     }
 
-    avgLat = s.lat_num / s.lat_den;
-    avgLng = s.lng_num / s.lng_den;
-    probability = s.sum_prob/nearDots.length;
+    var avgLat = s.lat_num / s.lat_den;
+    var avgLng = s.lng_num / s.lng_den;
+    var probability = s.sum_prob / nearDots.length;
+    var avgRadius = s.sum_distance / nearDots.length;
 
-    return {lng: avgLng, lat: avgLat, probability: probability};
+    return {
+        lng: avgLng,
+        lat: avgLat,
+        probability: probability,
+        avgRadius: avgRadius,
+        maxDistance: s.maxDistance,
+        minDistance: s.minDistance
+    };
 }
 
 
 function corelateResult(beaconsWithRadiuses) {
-    var count = 50,
-        min = 0.3,
-        max = 2.8,
+    var count = 20,
+        min = 0.5,
+        max = 2.5,
         resultArr = [];
 
 
@@ -212,15 +225,14 @@ function corelateResult(beaconsWithRadiuses) {
         // change radiuses
         var currRadiusKoeficient = (i + 1) * step;
         for (var j = 0; j < clonedBeaconsWithRadiuses.length; j++) {
-            var currRadius = beaconsWithRadiuses[j].radius+1-1;
+            var currRadius = beaconsWithRadiuses[j].radius;
             clonedBeaconsWithRadiuses[j].radius = currRadius * currRadiusKoeficient;
         }
         var realPosition = detectRealPosition(clonedBeaconsWithRadiuses);
-        realPosition.radiusKoeficient = currRadiusKoeficient;
-        if(!isNaN(realPosition.probability)){
+        if (realPosition != null && !isNaN(realPosition.probability)) {
+            realPosition.radiusKoeficient = currRadiusKoeficient;
             resultArr.push(realPosition);
         }
-
 
 
     }
@@ -232,16 +244,14 @@ function corelateResult(beaconsWithRadiuses) {
 }
 
 
-
-function getIntersectDot(line1, line2)
-{
+function getIntersectDot(line1, line2) {
 
     // возвращаем true если b находится между a и c,
     // исключаем случай, если a==b или b==c
     function isInBetween(a, b, c) {
         // возвращаем false, если b почти равна a или c.
         // для этого применяем значение с плавающей точкой 0.00000...0001
-        if (Math.abs(a-b) < 0.000001 || Math.abs(b-c) < 0.000001) {
+        if (Math.abs(a - b) < 0.000001 || Math.abs(b - c) < 0.000001) {
             return false;
         }
 
@@ -250,63 +260,36 @@ function getIntersectDot(line1, line2)
     }
 
 
-
-    // преобразуем линию line1 в общую форму уравнения прямой: Ax+By = C
-
-    // startPoint = D1
-    // endPoint = D2
-    // x = lng
-    // y = lat
-
-    line1.startPoint = line1.D1;
-    line1.startPoint.x = line1.D1.lng;
-    line1.startPoint.y = line1.D1.lat;
-
-    line1.endPoint = line1.D2;
-    line1.endPoint.x = line1.D2.lng;
-    line1.endPoint.y = line1.D2.lat;
-
-    line2.startPoint = line2.D1;
-    line2.startPoint.x = line2.D1.lng;
-    line2.startPoint.y = line2.D1.lat;
-
-    line2.endPoint = line2.D2;
-    line2.endPoint.x = line2.D2.lng;
-    line2.endPoint.y = line2.D2.lat;
-
-
-
-    var a1 = line1.endPoint.y - line1.startPoint.y;
-    var b1 = line1.startPoint.x - line1.endPoint.x;
-    var c1 = a1 * line1.startPoint.x + b1 * line1.startPoint.y;
+    var a1 = line1.D2.lat - line1.D1.lat;
+    var b1 = line1.D1.lng - line1.D2.lng;
+    var c1 = a1 * line1.D1.lng + b1 * line1.D1.lat;
 
     // преобразуем линию line2 в общую форму уравнения прямой: Ax+By = C
-    var a2 = line2.endPoint.y - line2.startPoint.y;
-    var b2 = line2.startPoint.x - line2.endPoint.x;
-    var c2 = a2 * line2.startPoint.x + b2 * line2.startPoint.y;
+    var a2 = line2.D2.lat - line2.D1.lat;
+    var b2 = line2.D1.lng - line2.D2.lng;
+    var c2 = a2 * line2.D1.lng + b2 * line2.D1.lat;
 
     // вычисляем точку пересечения
-    var d = a1*b2 - a2*b1;
+    var d = a1 * b2 - a2 * b1;
 
     // линии параллельны, если d = 0
     if (d == 0) {
         return false;
     }
     else {
-        var x = (b2*c1 - b1*c2) / d;
-        var y = (a1*c2 - a2*c1) / d;
-
+        var x = (b2 * c1 - b1 * c2) / d;
+        var y = (a1 * c2 - a2 * c1) / d;
 
 
         // проверяем, находится ли точка пересечения на обоих линиях
-        if ((isInBetween(line1.startPoint.x, x, line1.endPoint.x) ||
-            isInBetween(line1.startPoint.y, y, line1.endPoint.y)) &&
-            (isInBetween(line2.startPoint.x, x, line2.endPoint.x) ||
-            isInBetween(line2.startPoint.y, y, line2.endPoint.y)))
-        {
-            //return true;
-            return {lat: y, lng: x};
-        }
+        //if ((isInBetween(line1.D1.lng, x, line1.D2.lng) ||
+        //    isInBetween(line1.D1.lat, y, line1.D2.lat)) &&
+        //    (isInBetween(line2.D1.lng, x, line2.D2.lng) ||
+        //    isInBetween(line2.D1.lat, y, line2.D2.lat)))
+        //{
+        //    //return true;
+        //    return {lat: y, lng: x};
+        //}
 
         return {lat: y, lng: x};
 
@@ -314,33 +297,9 @@ function getIntersectDot(line1, line2)
 
     return false;
 }
-
-
-
-
-function arrayClone( arr ) {
-
-    var i, copy;
-
-    if( Array.isArray( arr ) ) {
-        copy = arr.slice( 0 );
-        for( i = 0; i < copy.length; i++ ) {
-            copy[ i ] = Utils.arrayClone( copy[ i ] );
-        }
-        return copy;
-    } else if( typeof arr === 'object' ) {
-        throw 'Cannot clone array containing an object!';
-    } else {
-        return arr;
-    }
-
-}
-
-
-
-
-
+//
+//
 //var beaconsWithRadiuses = buildBeaconsWithRadiusesArray(scannedBeaconsArr, existedBeaconsArr);
 //var realPosition = corelateResult(beaconsWithRadiuses);
-//var realPosition = detectRealPosition(beaconsWithRadiuses);
+////var realPosition = detectRealPosition(beaconsWithRadiuses);
 //realPosition;
